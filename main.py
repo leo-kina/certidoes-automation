e5497c0d00ea5d7b44f62fd000b2b826192cb9fa
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-12-09 11:20:32 -0300

    fix: bug

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-from core.driver import criar_driver
-from config.settings import SETTINGS
-from sites.certida_negativa_debitos_trabalhistas import emitir_cndt
+from dotenv import load_dotenv
+import os
 
+# Carrega .env usando caminho absoluto
+load_dotenv("C:/Users/leonardo.kina/Desktop/DUE_aut/.env")
 
-def executar(site, cnpj=None):
-    driver = criar_driver()
-
-    dados = {
-        "cnpj": cnpj,
-        "url": SETTINGS.get(site),
-    }
-
-    try:
-        if site == "cndt":
-            emitir_cndt(driver, dados)
-        else:
-            print("Site não configurado.")
-            return
-
-        print("✔️ Certidão emitida!")
-
-    except Exception as e:
-        print(f"Erro: {e}")
-
-    finally:
-        driver.quit()
-
-
-if __name__ == "__main__":
-    executar("cndt", cnpj="62285390000140")
+# Teste
+CNPJ_SIN = os.getenv("CNPJ_SINGULARE")
+print("DEBUG: CNPJ_SINGULARE =", CNPJ_SIN)
